8c77e62b694faeac0ac2c903be50931b4d90998d
ahmadmaulana730 <amkomarudin7@gmail.com>
2019-11-15 04:12:06 +0700

    menyelesaikan sidebar dan topbar untuk logout

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,9 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
     public function index(){
-
+        $data['title'] = 'Dasboard My Profile';
         $data['user']=$this->db->get_where('user',['email' =>$this->session->userdata('email')])->row_array();
-    
-        echo'selamat datang ' . $data['user']['name'];
+
+        $this->load->view('user/index', $data);
+
     }
 }
