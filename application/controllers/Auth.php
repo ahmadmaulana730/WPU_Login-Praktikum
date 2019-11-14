0ff5653e8e312b91b5883d6e9068f0189c6513e9
ahmadmaulana730 <amkomarudin7@gmail.com>
2019-11-14 19:54:56 +0700

    Memodifikasi tampilan login/register

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,8 +3,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
 	public function index()
 	{
+		$data['title']='WPU User Login';
+		$this->load->view('templates/auth_header',$data);
 		$this->load->view('auth/login');
+		$this->load->view('templates/auth_footer');
 	}
+
+	public function registration()
+	{
+		$this->form_validation->set_rules('email','Email','trim|required|valid_email');
+		$this->form_validation->set_rules('name','Name','trim|required');
+		$this->form_validation->set_rules('password','Password','trim|required');
+
+		if($this->form_validation->run()==false){
+			$data['title']='WPU User Registration';
+			$this->load->view('templates/auth_header',$data);
+			$this->load->view('auth/registration');
+			$this->load->view('templates/auth_footer');
+		}else{
+			echo 'data berhasil ditambahkan';
+		}
+	}
+
+
 }
